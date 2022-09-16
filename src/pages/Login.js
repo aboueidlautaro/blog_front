2f4e003df41cfc9845834efa96e1264fc2343f61
Lautaro Abou-Eid <70718664+aboueidlautaro@users.noreply.github.com>
2022-09-16 11:05:09 -0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ function Login() {
 
   const login = () => {
     const data = { username: username, password: password };
-    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+    axios.post("https://blog-jwt.herokuapp.com/auth/login", data).then((response) => {
       if (response.data.error) {
         setMessageForm("Contraseña incorrecta");
       } else {
